277151a3d4762728e2d12ed9daf5e489b5096a18
Rajdeep9854 <82280472+Rajdeep9854@users.noreply.github.com>
2023-09-12 06:23:40 +0000

    datatype summeery

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+// primitive 
+
+// 7  types  : String , Number , Boolean , null , undefined , Symbol, BigInt
+
+//javascript is dynamically typed language 
+// because we can not define type of a variable with our own it is defined dynamically when variable is stored 
+
+
+/*const id = Symbol("123");
+console.log(id);
+const anotherid = Symbol("123");
+console.log(anotherid);
+console.log(id===anotherid);*/
+
+
+//console.log(typeof bigNumber);
+
+
+// Reference type (Non premitive )
+
+// Arrays , Objects , Functions 
+
+const heros = ["saktiman" , "nagraj", "dogu"]
+const myObject = {
+    name : "Rajdeep",
+    age : 22
+}
+const myFunction = function(){
+    console.log("Hello World");
+}
+myFunction();
