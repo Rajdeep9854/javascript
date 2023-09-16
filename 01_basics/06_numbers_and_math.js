7d725c3431a9c8abb627fdfb8ff780dc90caa2ee
Rajdeep9854 <82280472+Rajdeep9854@users.noreply.github.com>
2023-09-16 05:57:20 +0000

    math & num

---
+++ 01_basics/06_numbers_and_math.js
@@ -0,0 +1,33 @@
+const score = 400;
+//console.log(score);
+
+const balance = new Number(100);
+//console.log(balance);
+
+//console.log( typeof balance.toString());
+//console.log(balance.toFixed(2));
+
+
+
+// toFixed and toPresission 
+const otherNumber = 23.8966
+
+//console.log(otherNumber.toPrecision());
+
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+//**********************************maths****************************** */
+
+/*console.log(Math.abs(-7));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));*/
+
+console.log(Math.floor((Math.random()*10)+1))
+
+let max =6
+let min = 1
+console.log(Math.floor(Math.random()*(max-min+1))+min);
