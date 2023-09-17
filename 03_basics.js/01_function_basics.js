9c66e2cc599f321b9d49627a56898e4bc4befefd
Rajdeep9854 <82280472+Rajdeep9854@users.noreply.github.com>
2023-09-17 09:38:34 +0000

    function basics

---
+++ 03_basics.js/01_function_basics.js
@@ -0,0 +1,26 @@
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
