a8a5e10116e8b5a15928fec4394a0b9622b4c758
Rajdeep9854 <82280472+Rajdeep9854@users.noreply.github.com>
2023-09-17 17:45:59 +0000

    function intermediate 4

--- 03_basics.js/01_function_basics.js
+++ 03_basics.js/01_function_basics.js
@@ -26,11 +26,32 @@ const result = addTwoNumbers(3, 5)
 // console.log("Result: ", result);
 
 
-function loginUserMessage(name="piyali"){ // this is default value 
+/*function loginUserMessage(name="piyali"){ // this is default value 
     if(name===undefined){
         console.log("please enter a username");
         return 
     }
     return `${name} just logged in `
 }
-console.log(loginUserMessage());
+//console.log(loginUserMessage());*/
+
+/*function calculateCarPrice(...num){// user of rest operator 
+    
+    return num
+}
+console.log(calculateCarPrice(200,500,700));
+
+function calculateCarPrice1(val1,val2,...num1){
+     return num1
+}
+console.log(calculateCarPrice1(200,300,700,2000));*/
+
+const user = {
+    userName :"rajdeep",
+    price : 299
+}
+
+function handleObject(anyObject){
+    console.log(`name of the user is ${anyObject.userName} and the price is ${anyObject.price}`);
+}
+handleObject(user);
